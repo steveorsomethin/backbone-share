522e37a36196c79e77ac353da0c8d4f366de1665
Steve McGuire <steveorsomethin@gmail.com>
2013-01-15 14:12:41 -0700

    Added more tests for incoming operations

--- test/SharedModel.hierarchical.js
+++ test/SharedModel.hierarchical.js
@@ -7,227 +7,353 @@ var TestChildModel = Backbone.SharedModel.extend({
 });
 
 var TestParentModel = Backbone.SharedModel.extend({
+	subDocTypes: {
+		objTest: TestChildModel
+	},
+
 	defaults: function() {
 		return {
 			strTest: 'abcdefg',
 			boolTest: false,
 			numTest: 50,
 			objTest: new TestChildModel({}, {
-				parent: this, 
+				parent: this,
 				documentPath: this.generateDocumentPath().concat(['objTest'])
 			})
 		};
 	}
 });
 
-describe('SharedModel', function() {
+describe('SharedModel - hierarchical', function() {
 	before(function(done) {
 		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
 			done();
 		});
 	});
 
-	describe('hierarchical', function() {
-		it('should emit si operation when adding text', function(done) {
-			new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should emit si operation when adding text', function(done) {
+		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
+			var model = this;
 
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['objTest', 'strTest', 7], si: 'hij'}]);
-						expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
-					});
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['objTest', 'strTest', 7], si: 'hij'}]);
+					expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
 				});
+			});
 
-				this.set('strTest', 'abcdefghij');
+			this.set('strTest', 'abcdefghij');
+		});
+	});
+
+	it('should emit sd operation when deleting text', function(done) {
+		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
+			var model = this;
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['objTest', 'strTest', 6], sd: 'g'}]);
+					expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
+				});
+			});
+
+			this.set('strTest', 'abcdef');
+		});
+	});
+
+	it('should emit si and sd operations when replacing text', function(done) {
+		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
+			var model = this;
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([
+						{p: ['objTest', 'strTest', 2], sd: 'cde'}, 
+						{p: ['objTest', 'strTest', 2], si: '123'}
+					]);
+					expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
+				});
+			});
+
+			this.set('strTest', 'ab123fg');
+		});
+	});
+
+	it('should emit oi and od operations when setting value to true', function(done) {
+		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
+			var model = this;
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['objTest', 'boolTest'], od: false, oi: true}]);
+					expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
+				});
+			});
+
+			this.set('boolTest', true);
+		});
+	});
+
+	it('should emit positive na operation when increasing a number', function(done) {
+		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
+			var model = this;
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['objTest', 'numTest'], na: 50}]);
+					expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
+				});
+			});
+
+			this.set('numTest', 100);
+		});
+	});
+
+	it('should emit positive na operation when decreasing a number', function(done) {
+		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
+			var model = this;
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['objTest', 'numTest'], na: -50}]);
+					expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
+				});
+			});
+
+			this.set('numTest', 0);
+		});
+	});
+
+	it('should emit oi operation when setting a child SharedModel', function(done) {
+		new TestParentModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['objTest2'], oi: model.get('objTest2').toJSON()}]);
+					expect(shareDoc.snapshot.objTest2).to.eql(model.get('objTest2').toJSON());
+				});
+			});
+
+			this.set('objTest2', new TestChildModel({}, {
+				parent: this,
+				documentPath: this.generateDocumentPath().concat(['objTest2'])
+			}));
+		});
+	});
+
+	it('should emit od operation when unsetting a child SharedModel', function(done) {
+		new TestParentModel().on('share:connected', function(shareDoc) {
+			var model = this;
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['objTest'], od: model.previous('objTest').toJSON()}]);
+					expect(shareDoc.snapshot.objTest).to.eql(undefined);
+				});
+			});
+
+			this.unset('objTest');
+		});
+	});
+
+	it('should emit oi and od operations when replacing a child SharedModel', function(done) {
+		new TestParentModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{
+						p: ['objTest'], 
+						oi: model.get('objTest').toJSON(),
+						od: model.previous('objTest').toJSON()
+					}]);
+					expect(shareDoc.snapshot.objTest).to.eql(model.get('objTest').toJSON());
+				});
+			});
+
+			this.set('objTest', new TestChildModel({}, {
+				parent: this,
+				documentPath: this.generateDocumentPath().concat(['objTest'])
+			}));
+		});
+	});
+
+	it('should emit oi operation when setting a child JS object', function(done) {
+		new TestParentModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['objTest2'], oi: model.get('objTest2')}]);
+					expect(shareDoc.snapshot.objTest2).to.eql(model.get('objTest2'));
+				});
+			});
+
+			this.set('objTest2', new TestChildModel({}, {
+				parent: this,
+				documentPath: this.generateDocumentPath().concat(['objTest2'])
+			}).toJSON());
+		});
+	});
+
+	it('should emit od operation when unsetting a child JS object', function(done) {
+		new TestParentModel({
+			objTest: new TestChildModel().toJSON()
+		}).on('share:connected', function(shareDoc) {
+			var model = this;
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['objTest'], od: model.previous('objTest')}]);
+					expect(shareDoc.snapshot.objTest).to.eql(undefined);
+				});
+			});
+
+			this.unset('objTest');
+		});
+	});
+
+	it('should emit oi and od operations when replacing a child JS object', function(done) {
+		new TestParentModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{
+						p: ['objTest'], 
+						oi: model.get('objTest'),
+						od: model.previous('objTest').toJSON()
+					}]);
+					expect(shareDoc.snapshot.objTest).to.eql(model.get('objTest'));
+				});
+			});
+
+			this.set('objTest', new TestChildModel({}, {
+				parent: this,
+				documentPath: this.generateDocumentPath().concat(['objTest'])
+			}).toJSON());
+		});
+	});
+
+	it('should update on incoming si operation', function(done) {
+		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('strTest')).to.eql('abcdefghij');
+				});
+			});
+
+			this._handleOperation({p: ['objTest', 'strTest', 7], si: 'hij'});
+		});
+	});
+
+	it('should update on incoming sd operation', function(done) {
+		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('strTest')).to.eql('abefg');
+				});
+			});
+
+			this._handleOperation({p: ['objTest', 'strTest', 2], sd: 'cd'});
+		});
+	});
+
+	it('should update on incoming oi/od operation with true', function(done) {
+		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('boolTest')).to.eql(true);
+				});
+			});
+
+			this._handleOperation({p: ['objTest', 'boolTest'], od: false, oi: true});
+		});
+	});
+
+	it('should update on incoming oi/od operation with false', function(done) {
+		new TestParentModel({
+			objTest: new TestChildModel({boolTest: true}, {defer: true})
+		}).on('share:connected', function(shareDoc) {
+			var model = this.get('objTest');
+			model.documentPath = this.generateDocumentPath().concat(['objTest']);
+			model.setParent(this);
+
+			model.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('boolTest')).to.eql(false);
+				});
+			});
+
+			model._handleOperation({p: ['objTest', 'boolTest'], od: true, oi: false});
+		});
+	});
+
+	it('should update on incoming positive na operation', function(done) {
+		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('numTest')).to.eql(100);
+				});
+			});
+
+			this._handleOperation({p: ['objTest', 'numTest'], na: 50});
+		});
+	});
+
+	it('should update on incoming negative na operation', function(done) {
+		new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('numTest')).to.eql(0);
+				});
+			});
+
+			this._handleOperation({p: ['objTest', 'numTest'], na: -50});
+		});
+	});
+
+	it('should update on incoming oi operation with SharedModel', function(done) {
+		new TestParentModel().on('share:connected', function(shareDoc) {
+			var model = this;
+			var newModel = new TestChildModel();
+
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('objTest').toJSON()).to.eql(newModel.toJSON());
+				});
+			});
+
+			this._handleOperation({
+				p: ['objTest'],
+				oi: newModel.toJSON()
 			});
 		});
+	});
 
-		it('should emit sd operation when deleting text', function(done) {
-			new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should update on incoming od operation with SharedModel', function(done) {
+		new TestParentModel().on('share:connected', function(shareDoc) {
+			var model = this;
+			var newModel = new TestChildModel();
 
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['objTest', 'strTest', 6], sd: 'g'}]);
-						expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
-					});
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('objTest')).to.eql(undefined);
 				});
-
-				this.set('strTest', 'abcdef');
 			});
-		});
 
-		it('should emit si and sd operations when replacing text', function(done) {
-			new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
-				var model = this;
-
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([
-							{p: ['objTest', 'strTest', 2], sd: 'cde'}, 
-							{p: ['objTest', 'strTest', 2], si: '123'}
-						]);
-						expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
-					});
-				});
-
-				this.set('strTest', 'ab123fg');
-			});
-		});
-
-		it('should emit oi and od operations when setting value to true', function(done) {
-			new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
-				var model = this;
-
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['objTest', 'boolTest'], od: false, oi: true}]);
-						expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
-					});
-				});
-
-				this.set('boolTest', true);
-			});
-		});
-
-		it('should emit positive na operation when increasing a number', function(done) {
-			new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
-				var model = this;
-
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['objTest', 'numTest'], na: 50}]);
-						expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
-					});
-				});
-
-				this.set('numTest', 100);
-			});
-		});
-
-		it('should emit positive na operation when decreasing a number', function(done) {
-			new TestParentModel().get('objTest').on('share:connected', function(shareDoc) {
-				var model = this;
-
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['objTest', 'numTest'], na: -50}]);
-						expect(shareDoc.snapshot.objTest).to.eql(model.toJSON());
-					});
-				});
-
-				this.set('numTest', 0);
-			});
-		});
-
-		it('should emit oi operation when setting a child SharedModel', function(done) {
-			new TestParentModel().on('share:connected', function(shareDoc) {
-				var model = this;
-
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['objTest2'], oi: model.get('objTest2').toJSON()}]);
-						expect(shareDoc.snapshot.objTest2).to.eql(model.get('objTest2').toJSON());
-					});
-				});
-
-				this.set('objTest2', new TestChildModel({}, {
-					parent: this,
-					documentPath: this.generateDocumentPath().concat(['objTest2'])
-				}));
-			});
-		});
-
-		it('should emit od operation when unsetting a child SharedModel', function(done) {
-			new TestParentModel().on('share:connected', function(shareDoc) {
-				var model = this;
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['objTest'], od: model.previous('objTest').toJSON()}]);
-						expect(shareDoc.snapshot.objTest).to.eql(undefined);
-					});
-				});
-
-				this.unset('objTest');
-			});
-		});
-
-		it('should emit oi and od operations when replacing a child SharedModel', function(done) {
-			new TestParentModel().on('share:connected', function(shareDoc) {
-				var model = this;
-
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{
-							p: ['objTest'], 
-							oi: model.get('objTest').toJSON(),
-							od: model.previous('objTest').toJSON()
-						}]);
-						expect(shareDoc.snapshot.objTest).to.eql(model.get('objTest').toJSON());
-					});
-				});
-
-				this.set('objTest', new TestChildModel({}, {
-					parent: this,
-					documentPath: this.generateDocumentPath().concat(['objTest'])
-				}));
-			});
-		});
-
-		it('should emit oi operation when setting a child JS object', function(done) {
-			new TestParentModel().on('share:connected', function(shareDoc) {
-				var model = this;
-
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['objTest2'], oi: model.get('objTest2')}]);
-						expect(shareDoc.snapshot.objTest2).to.eql(model.get('objTest2'));
-					});
-				});
-
-				this.set('objTest2', new TestChildModel({}, {
-					parent: this,
-					documentPath: this.generateDocumentPath().concat(['objTest2'])
-				}).toJSON());
-			});
-		});
-
-		it('should emit od operation when unsetting a child JS object', function(done) {
-			new TestParentModel({
-				objTest: new TestChildModel().toJSON()
-			}).on('share:connected', function(shareDoc) {
-				var model = this;
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['objTest'], od: model.previous('objTest')}]);
-						expect(shareDoc.snapshot.objTest).to.eql(undefined);
-					});
-				});
-
-				this.unset('objTest');
-			});
-		});
-
-		it('should emit oi and od operations when replacing a child JS object', function(done) {
-			new TestParentModel().on('share:connected', function(shareDoc) {
-				var model = this;
-
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{
-							p: ['objTest'], 
-							oi: model.get('objTest'),
-							od: model.previous('objTest').toJSON()
-						}]);
-						expect(shareDoc.snapshot.objTest).to.eql(model.get('objTest'));
-					});
-				});
-
-				this.set('objTest', new TestChildModel({}, {
-					parent: this,
-					documentPath: this.generateDocumentPath().concat(['objTest'])
-				}).toJSON());
+			this._handleOperation({
+				p: ['objTest'],
+				od: this.get('objTest').toJSON(),
 			});
 		});
 	});
--- test/SharedModel.non-hierarchical.js
+++ test/SharedModel.non-hierarchical.js
@@ -6,201 +6,199 @@ var TestModel = Backbone.SharedModel.extend({
 	}
 });
 
-describe('SharedModel', function() {
+describe('SharedModel - non-hierarchical', function() {
 	before(function(done) {
 		new TestModel().on('share:connected', function(shareDoc) {
 			done();
 		});
 	});
 
-	describe('non-hierarchical', function() {
-		it('should emit si operation when adding text', function(done) {
-			new TestModel().on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should emit si operation when adding text', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
 
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['strTest', 7], si: 'hij'}]);
-						expect(shareDoc.snapshot).to.eql(model.toJSON());
-					});
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['strTest', 7], si: 'hij'}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
 				});
-
-				this.set('strTest', 'abcdefghij');
 			});
+
+			this.set('strTest', 'abcdefghij');
 		});
+	});
 
-		it('should emit sd operation when deleting text', function(done) {
-			new TestModel().on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should emit sd operation when deleting text', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
 
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['strTest', 6], sd: 'g'}]);
-						expect(shareDoc.snapshot).to.eql(model.toJSON());
-					});
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['strTest', 6], sd: 'g'}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
 				});
-
-				this.set('strTest', 'abcdef');
 			});
+
+			this.set('strTest', 'abcdef');
 		});
+	});
 
-		it('should emit si and sd operations when replacing text', function(done) {
-			new TestModel().on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should emit si and sd operations when replacing text', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
 
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([ { p: [ 'strTest', 2 ], sd: 'cde' }, { p: [ 'strTest', 2 ], si: '123' } ]);
-						expect(shareDoc.snapshot).to.eql(model.toJSON());
-					});
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([ { p: [ 'strTest', 2 ], sd: 'cde' }, { p: [ 'strTest', 2 ], si: '123' } ]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
 				});
-
-				this.set('strTest', 'ab123fg');
 			});
+
+			this.set('strTest', 'ab123fg');
 		});
+	});
 
-		it('should emit oi and od operations when setting value to true', function(done) {
-			new TestModel().on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should emit oi and od operations when setting value to true', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
 
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['boolTest'], od: false, oi: true}]);
-						expect(shareDoc.snapshot).to.eql(model.toJSON());
-					});
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['boolTest'], od: false, oi: true}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
 				});
-
-				this.set('boolTest', true);
 			});
+
+			this.set('boolTest', true);
 		});
+	});
 
-		it('should emit oi and od operations when setting value to false', function(done) {
-			new TestModel({boolTest: true}).on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should emit oi and od operations when setting value to false', function(done) {
+		new TestModel({boolTest: true}).on('share:connected', function(shareDoc) {
+			var model = this;
 
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['boolTest'], od: true, oi: false}]);
-						expect(shareDoc.snapshot).to.eql(model.toJSON());
-					});
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['boolTest'], od: true, oi: false}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
 				});
-
-				this.set('boolTest', false);
 			});
+
+			this.set('boolTest', false);
 		});
+	});
 
-		it('should emit positive na operation when increasing a number', function(done) {
-			new TestModel().on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should emit positive na operation when increasing a number', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
 
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['numTest'], na: 50}]);
-						expect(shareDoc.snapshot).to.eql(model.toJSON());
-					});
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['numTest'], na: 50}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
 				});
-
-				this.set('numTest', 100);
 			});
+
+			this.set('numTest', 100);
 		});
+	});
 
-		it('should emit negative na operation when decreasing a number', function(done) {
-			new TestModel().on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should emit negative na operation when decreasing a number', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
 
-				shareDoc.on('change', function(ops) {
-					asyncAssert(done, function() {
-						expect(ops).to.eql([{p: ['numTest'], na: -50}]);
-						expect(shareDoc.snapshot).to.eql(model.toJSON());
-					});
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['numTest'], na: -50}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
 				});
-
-				this.set('numTest', 0);
 			});
+
+			this.set('numTest', 0);
 		});
+	});
 
-		it('should update on incoming si operation', function(done) {
-			new TestModel().on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should update on incoming si operation', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
 
-				this.on('change', function() {
-					asyncAssert(done, function() {
-						expect(model.get('strTest')).to.eql('abcdefghij');
-					});
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('strTest')).to.eql('abcdefghij');
 				});
-
-				this._handleOperation({p: ['strTest', 7], si: 'hij'});
 			});
+
+			this._handleOperation({p: ['strTest', 7], si: 'hij'});
 		});
+	});
 
-		it('should update on incoming sd operation', function(done) {
-			new TestModel().on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should update on incoming sd operation', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
 
-				this.on('change', function() {
-					asyncAssert(done, function() {
-						expect(model.get('strTest')).to.eql('abefg');
-					});
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('strTest')).to.eql('abefg');
 				});
-
-				this._handleOperation({p: ['strTest', 2], sd: 'cd'});
 			});
+
+			this._handleOperation({p: ['strTest', 2], sd: 'cd'});
 		});
+	});
 
-		it('should update on incoming oi/od operation with true', function(done) {
-			new TestModel().on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should update on incoming oi/od operation with true', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
 
-				this.on('change', function() {
-					asyncAssert(done, function() {
-						expect(model.get('boolTest')).to.eql(true);
-					});
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('boolTest')).to.eql(true);
 				});
-
-				this._handleOperation({p: ['boolTest'], od: false, oi: true});
 			});
+
+			this._handleOperation({p: ['boolTest'], od: false, oi: true});
 		});
+	});
 
-		it('should update on incoming oi/od operation with false', function(done) {
-			new TestModel({boolTest: true}).on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should update on incoming oi/od operation with false', function(done) {
+		new TestModel({boolTest: true}).on('share:connected', function(shareDoc) {
+			var model = this;
 
-				this.on('change', function() {
-					asyncAssert(done, function() {
-						expect(model.get('boolTest')).to.eql(false);
-					});
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('boolTest')).to.eql(false);
 				});
-
-				this._handleOperation({p: ['boolTest'], od: true, oi: false});
 			});
+
+			this._handleOperation({p: ['boolTest'], od: true, oi: false});
 		});
+	});
 
-		it('should update on incoming positive na operation', function(done) {
-			new TestModel().on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should update on incoming positive na operation', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
 
-				this.on('change', function() {
-					asyncAssert(done, function() {
-						expect(model.get('numTest')).to.eql(100);
-					});
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('numTest')).to.eql(100);
 				});
-
-				this._handleOperation({p: ['numTest'], na: 50});
 			});
+
+			this._handleOperation({p: ['numTest'], na: 50});
 		});
+	});
 
-		it('should update on incoming negative na operation', function(done) {
-			new TestModel().on('share:connected', function(shareDoc) {
-				var model = this;
+	it('should update on incoming negative na operation', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
 
-				this.on('change', function() {
-					asyncAssert(done, function() {
-						expect(model.get('numTest')).to.eql(0);
-					});
+			this.on('change', function() {
+				asyncAssert(done, function() {
+					expect(model.get('numTest')).to.eql(0);
 				});
-
-				this._handleOperation({p: ['numTest'], na: -50});
 			});
+
+			this._handleOperation({p: ['numTest'], na: -50});
 		});
 	});
 });
