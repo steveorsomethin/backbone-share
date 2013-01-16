8d1eb5e5458130028ae260d42cbff971b6b58055
Steve McGuire <steveorsomethin@gmail.com>
2013-01-15 20:59:17 -0700

    Added undo/redo tests for simple types

--- test/SharedModel.non-hierarchical.js
+++ test/SharedModel.non-hierarchical.js
@@ -49,7 +49,7 @@ describe('SharedModel - non-hierarchical', function() {
 
 			shareDoc.on('change', function(ops) {
 				asyncAssert(done, function() {
-					expect(ops).to.eql([ { p: [ 'strTest', 2 ], sd: 'cde' }, { p: [ 'strTest', 2 ], si: '123' } ]);
+					expect(ops).to.eql([{p: ['strTest', 2], sd: 'cde'}, {p: ['strTest', 2 ], si: '123'}]);
 					expect(shareDoc.snapshot).to.eql(model.toJSON());
 				});
 			});
@@ -199,6 +199,288 @@ describe('SharedModel - non-hierarchical', function() {
 			});
 
 			this._handleOperation({p: ['numTest'], na: -50});
+		});
+	});
+
+	it('should reset the undo stack on new operation', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('strTest', 'ab123fg');
+			this.set('strTest', 'ab1234fg');
+			this.undo();
+
+			asyncAssert(done, function() {
+				expect(model.get('strTest')).to.eql('ab123fg');
+				expect(shareDoc.snapshot).to.eql(model.toJSON());
+
+				model.set('strTest', 'abcdefghij');
+
+				expect(model.get('strTest')).to.eql('abcdefghij');
+				expect(shareDoc.snapshot).to.eql(model.toJSON());
+			
+				expect(model.undoStack).to.eql([
+					[
+						{p: ['strTest', 2], sd: 'cde'},
+						{p: ['strTest', 2], si: '123'}
+					],
+					[
+						{p: ['strTest', 2], sd: '123fg'},
+						{p: ['strTest', 2], si: 'cdefghij'}
+					]
+				]);
+			});
+		});
+	});
+
+	it('should emit sd operation on undo of si operation', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('strTest', 'abcdefghij');
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['strTest', 7], sd: 'hij'}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+					expect(model.get('strTest')).to.eql('abcdefg');
+				});
+			});
+
+			this.undo();
+		});
+	});
+
+	it('should emit si operation on redo of si operation', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('strTest', 'abcdefghij');
+			this.undo();
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['strTest', 7], si: 'hij'}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+					expect(model.get('strTest')).to.eql('abcdefghij');
+				});
+			});
+
+			this.redo();
+		});
+	});
+
+	it('should emit si operation on undo of sd operation', function(done) {
+		new TestModel({strTest: 'abcdefghij'}).on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('strTest', 'abcdefg');
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['strTest', 7], si: 'hij'}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+					expect(model.get('strTest')).to.eql('abcdefghij');
+				});
+			});
+
+			this.undo();
+		});
+	});
+
+	it('should emit sd operation on redo of sd operation', function(done) {
+		new TestModel({strTest: 'abcdefghij'}).on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('strTest', 'abcdefg');
+			this.undo();
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['strTest', 7], sd: 'hij'}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+					expect(model.get('strTest')).to.eql('abcdefg');
+				});
+			});
+
+			this.redo();
+		});
+	});
+
+	it('should emit sd and si operations on undo of string replace', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('strTest', 'ab123fg');
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['strTest', 2 ], sd: '123'}, {p: ['strTest', 2 ], si: 'cde'}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+					expect(model.get('strTest')).to.eql('abcdefg');
+				});
+			});
+
+			this.undo();
+		});
+	});
+
+	it('should emit sd and si operations on redo of string replace', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('strTest', 'ab123fg');
+			this.undo();
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['strTest', 2 ], sd: 'cde'}, {p: ['strTest', 2 ], si: '123'}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+					expect(model.get('strTest')).to.eql('ab123fg');
+				});
+			});
+
+			this.redo();
+		});
+	});
+
+	it('should emit od and oi operations on undo of boolean true', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('boolTest', true);
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['boolTest'], od: true, oi: false}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+				});
+			});
+
+			this.undo();
+		});
+	});
+
+	it('should emit od and oi operations on redo of boolean true', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('boolTest', true);
+			this.undo();
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['boolTest'], od: false, oi: true}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+				});
+			});
+
+			this.redo();
+		});
+	});
+
+	it('should emit od and oi operations on undo of boolean false', function(done) {
+		new TestModel({boolTest: true}).on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('boolTest', false);
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['boolTest'], od: false, oi: true}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+				});
+			});
+
+			this.undo();
+		});
+	});
+
+	it('should emit od and oi operations on redo of boolean false', function(done) {
+		new TestModel({boolTest: true}).on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('boolTest', false);
+			this.undo();
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['boolTest'], od: true, oi: false}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+				});
+			});
+
+			this.redo();
+		});
+	});
+
+	it('should emit positive na operation on undo of negative na', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('numTest', 0);
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['numTest'], na: 50}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+				});
+			});
+
+			this.undo();
+		});
+	});
+
+	it('should emit negative na operation on redo of negative na', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('numTest', 0);
+			this.undo();
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['numTest'], na: -50}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+				});
+			});
+
+			this.redo();
+		});
+	});
+
+	it('should emit negative na operation on undo of positive na', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('numTest', 100);
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['numTest'], na: -50}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+				});
+			});
+
+			this.undo();
+		});
+	});
+
+	it('should emit positive na operation on redo of positive na', function(done) {
+		new TestModel().on('share:connected', function(shareDoc) {
+			var model = this;
+
+			this.set('numTest', 100);
+			this.undo();
+
+			shareDoc.on('change', function(ops) {
+				asyncAssert(done, function() {
+					expect(ops).to.eql([{p: ['numTest'], na: 50}]);
+					expect(shareDoc.snapshot).to.eql(model.toJSON());
+				});
+			});
+
+			this.redo();
 		});
 	});
 });
