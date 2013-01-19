70ac169959348ffeacd1c12e97b22d12fdb99ec6
Steve McGuire <steveorsomethin@gmail.com>
2013-01-19 11:21:09 -0700

    Added undo/redo SharedCollection tests

--- test/SharedCollection.js
+++ test/SharedCollection.js
@@ -52,6 +52,57 @@
 			});
 		});
 
+		it('should emit ld operations on undo of adding models', function(done) {
+			new TestCollection().share(function(error, root) {
+				var collection = this,
+					newModels = [new TestChildModel(), new TestChildModel(), new TestChildModel()];
+
+				this.add(newModels);
+
+				this.shareDoc.on('change', function(ops) {
+					asyncAssert(done, function() {
+						expect(ops.reverse()).to.eql(newModels.map(
+							function(model, i) {
+								return {
+									p: [i],
+									ld: model.toJSON()
+								};
+							})
+						);
+						expect(collection.shareDoc.snapshot).to.eql(collection.toJSON());
+					});
+				});
+
+				this.undo();
+			});
+		});
+
+		it('should emit li operations on redo of adding models', function(done) {
+			new TestCollection().share(function(error, root) {
+				var collection = this,
+					newModels = [new TestChildModel(), new TestChildModel(), new TestChildModel()];
+
+				this.add(newModels);
+				this.undo();
+
+				this.shareDoc.on('change', function(ops) {
+					asyncAssert(done, function() {
+						expect(ops).to.eql(newModels.map(
+							function(model, i) {
+								return {
+									p: [i],
+									li: model.toJSON()
+								};
+							})
+						);
+						expect(collection.shareDoc.snapshot).to.eql(collection.toJSON());
+					});
+				});
+
+				this.redo();
+			});
+		});
+
 		it('should emit ld operations when removing models', function(done) {
 			new TestCollection([new TestChildModel(), new TestChildModel(), new TestChildModel()]).share(function(error, root) {
 				var collection = this,
@@ -75,7 +126,58 @@
 			});
 		});
 
-		it('should add on incoming li operation', function(done) {
+		it('should emit li operations on undo of removing models', function(done) {
+			new TestCollection([new TestChildModel(), new TestChildModel(), new TestChildModel()]).share(function(error, root) {
+				var collection = this,
+					models = this.toJSON();
+
+				this.remove(this.models);
+
+				this.shareDoc.on('change', function(ops) {
+					asyncAssert(done, function() {
+						expect(ops).to.eql(models.map(
+							function(model, i) {
+								return {
+									p: [i],
+									li: model
+								};
+							})
+						);
+						expect(collection.shareDoc.snapshot).to.eql(collection.toJSON());
+					});
+				});
+
+				this.undo();
+			});
+		});
+
+		it('should emit ld operations on redo of removing models', function(done) {
+			new TestCollection([new TestChildModel(), new TestChildModel(), new TestChildModel()]).share(function(error, root) {
+				var collection = this,
+					models = this.toJSON();
+
+				this.remove(this.models);
+				this.undo();
+
+				this.shareDoc.on('change', function(ops) {
+					asyncAssert(done, function() {
+						expect(ops.reverse()).to.eql(models.map(
+							function(model, i) {
+								return {
+									p: [i],
+									ld: model
+								};
+							})
+						);
+						expect(collection.shareDoc.snapshot).to.eql(collection.toJSON());
+					});
+				});
+
+				this.redo();
+			});
+		});
+
+		it('should add elements on incoming li operation', function(done) {
 			new TestCollection([new TestChildModel(), new TestChildModel()]).share(function(error, root) {
 				var collection = this,
 					newModel = new TestChildModel();
@@ -90,7 +192,7 @@
 			});
 		});
 
-		it('should remove on incoming ld operation', function(done) {
+		it('should remove elements on incoming ld operation', function(done) {
 			new TestCollection([new TestChildModel(), new TestChildModel()]).share(function(error, root) {
 				var collection = this,
 					model = this.at(1);
